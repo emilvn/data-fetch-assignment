5fd2389265958a00495e7e93a3775c0170774df3
emilvn <emilvnielsen@hotmail.com>
2023-03-20 17:24:09 +0100

    added more characters

--- script.js
+++ script.js
@@ -2,40 +2,39 @@
 window.addEventListener("load", main);
 
 const characterContainer = document.querySelector("#characters");
-
+const dialogContainer = document.querySelector("#dialog-grid");
+/* =========== Main =========== */
 async function main() {
-    const characters = await loadJSON();
+    const characters = await fetchData();
     showCharacters(characters);
 }
-
-async function loadJSON() {
+/* =========== Fetch data =========== */
+async function fetchData() {
     const response = await fetch("data/tpb.json");
     const data = await response.json();
     return data;
 }
-
+/* =========== Display data =========== */
 function showCharacters(data) {
     for (const obj of data) {
         const myHTML = /*html*/`
         <article>
             <img src=${obj["image"]}>
             <h2>${obj["name"]}</h2>
-            <p>Age: ${getCurrentAge(obj["birthdate"])}</p>
-            <p>Gender: ${capitalize(obj["gender"])}</p>
-            <p>Favorite food/drink: ${capitalize(obj["favorite"])}</p>
-            <p>Role: ${capitalize(obj["role"])}</p>
+            <p>${capitalize(obj["role"])}</p>
         </article>
     `
-    characterContainer.insertAdjacentHTML("beforeend", myHTML);
+        characterContainer.insertAdjacentHTML("beforeend", myHTML);
+        characterContainer.querySelector("article:last-child").addEventListener("click", showDialog);
     } 
 }
-
+/* =========== Fortmat date correctly for Date object =========== */
 function formatDate(date) {
     let dateArr = date.split("-");
     dateArr = dateArr.reverse();
     return dateArr.join("-");
 }
-
+/* =========== Calculate age from date of birth =========== */
 function getCurrentAge(birthdate) {
     const today = new Date(Date.now());
     const dateOfBirth = new Date(formatDate(birthdate));
@@ -46,7 +45,42 @@ function getCurrentAge(birthdate) {
         return today.getFullYear() - dateOfBirth.getFullYear();
     }
 }
-
+/* =========== Text edit functions =========== */
 function capitalize(name) {
     return name[0].toUpperCase() + name.substring(1).toLowerCase();
+}
+
+function showDialog() {
+    showDialogCharacter(this);
+    document.querySelector("#character-dialog").showModal();
+}
+
+async function showDialogCharacter(article) {
+    const selectedCharacterName = article.querySelector("h2").textContent;
+    const characterObj = await findCharacter(selectedCharacterName);
+    const myHTML = /*html*/`
+            <figure>
+                <img src=${characterObj["image"]}>
+            </figure>
+            <article>
+                <h2>${characterObj["name"]}</h2>
+                <p id="dialog-quote">"${characterObj["quote"]}"</p>
+                <h3>Personal info</h3>
+                <p>Birthdate: ${characterObj["birthdate"]}</p>
+                <p>Age: ${getCurrentAge(characterObj["birthdate"])}</p>
+                <p>Gender: ${capitalize(characterObj["gender"])}</p>
+                <p>Favorite food/drink: ${capitalize(characterObj["favorite"])}</p>
+                <p>Role: ${capitalize(characterObj["role"])}</p>
+                <p>Favorite passtime: ${capitalize(characterObj["passtime"])}</p>
+                <p>Sport of choice: ${capitalize(characterObj["sport"])}</p>
+            </article>
+    `;
+    dialogContainer.innerHTML = myHTML;
+}
+
+async function findCharacter(name) {
+    const characters = await fetchData();
+    for (const obj of characters) {
+        if (obj["name"] === name) return obj;
+    }
 }
