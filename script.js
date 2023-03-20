40237a8376e23601daf721790a69c9545dc8832b
emilvn <emilvnielsen@hotmail.com>
2023-03-20 18:00:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function showCharacters(data) {
             <h2>${obj["name"]}</h2>
             <p>${capitalize(obj["role"])}</p>
         </article>
-    `
+    `;
         characterContainer.insertAdjacentHTML("beforeend", myHTML);
         characterContainer.querySelector("article:last-child").addEventListener("click", showDialog);
     } 
@@ -33,6 +33,41 @@ function formatDate(date) {
     let dateArr = date.split("-");
     dateArr = dateArr.reverse();
     return dateArr.join("-");
+}
+
+function showDialog() {
+    showDialogCharacter(this);
+    document.querySelector("#character-dialog").showModal();
+}
+async function showDialogCharacter(article) {
+    const selectedCharacterName = article.querySelector("h2").textContent;
+    const characterObj = await findCharacter(selectedCharacterName);
+    const myHTML = /*html*/`
+            <figure>
+                <img src=${characterObj["image"]}>
+            </figure>
+            <article>
+                <h2>${characterObj["name"]}</h2>
+                <p id="dialog-quote">"${characterObj["quote"]}"</p>
+                <h3>Personal info</h3>
+                <ul>
+                    <li>Birthdate: ${characterObj["birthdate"]}</li>
+                    <li>Age: ${getCurrentAge(characterObj["birthdate"])}</li>
+                    <li>Gender: ${capitalize(characterObj["gender"])}</li>
+                    <li>Favorite food/drink: ${capitalize(characterObj["favorite"])}</li>
+                    <li>Role: ${capitalize(characterObj["role"])}</li>
+                    <li>Favorite passtime: ${capitalize(characterObj["passtime"])}</li>
+                    <li>Sport of choice: ${capitalize(characterObj["sport"])}</li>
+                </ul>
+            </article>
+    `;
+    dialogContainer.innerHTML = myHTML;
+}
+async function findCharacter(name) {
+    const characters = await fetchData();
+    for (const obj of characters) {
+        if (obj["name"] === name) return obj;
+    }
 }
 /* =========== Calculate age from date of birth =========== */
 function getCurrentAge(birthdate) {
@@ -48,39 +83,4 @@ function getCurrentAge(birthdate) {
 /* =========== Text edit functions =========== */
 function capitalize(name) {
     return name[0].toUpperCase() + name.substring(1).toLowerCase();
-}
-
-function showDialog() {
-    showDialogCharacter(this);
-    document.querySelector("#character-dialog").showModal();
-}
-
-async function showDialogCharacter(article) {
-    const selectedCharacterName = article.querySelector("h2").textContent;
-    const characterObj = await findCharacter(selectedCharacterName);
-    const myHTML = /*html*/`
-            <figure>
-                <img src=${characterObj["image"]}>
-            </figure>
-            <article>
-                <h2>${characterObj["name"]}</h2>
-                <p id="dialog-quote">"${characterObj["quote"]}"</p>
-                <h3>Personal info</h3>
-                <p>Birthdate: ${characterObj["birthdate"]}</p>
-                <p>Age: ${getCurrentAge(characterObj["birthdate"])}</p>
-                <p>Gender: ${capitalize(characterObj["gender"])}</p>
-                <p>Favorite food/drink: ${capitalize(characterObj["favorite"])}</p>
-                <p>Role: ${capitalize(characterObj["role"])}</p>
-                <p>Favorite passtime: ${capitalize(characterObj["passtime"])}</p>
-                <p>Sport of choice: ${capitalize(characterObj["sport"])}</p>
-            </article>
-    `;
-    dialogContainer.innerHTML = myHTML;
-}
-
-async function findCharacter(name) {
-    const characters = await fetchData();
-    for (const obj of characters) {
-        if (obj["name"] === name) return obj;
-    }
 }
