8c76f578e3ff69ea8901963aa29cabd1648c1d0c
emilvn <emilvnielsen@hotmail.com>
2023-03-20 09:45:37 +0100

    added css grid

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+window.addEventListener("load", main);
+
+async function main() {
+    const characters = await loadJSON();
+}
+
+async function loadJSON() {
+    
+}
+
+function showCharacters() {
+    
+}
