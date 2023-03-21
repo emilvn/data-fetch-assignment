ebec11c29d167f1618b2b042f05515bb6b1fe84b
emilvn <emilvnielsen@hotmail.com>
2023-03-21 17:10:57 +0100

    added chef object

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function main() {
 
 /* =========== Fetch character data =========== */
 async function fetchData() {
-    const DATA_URL = "data/tpb.json";
+    const DATA_URL = "data/southpark.json";
     try {
         const response = await fetch(DATA_URL);
         if (!response.ok) {
@@ -36,7 +36,7 @@ function showCharacter(obj) {
         <article>
             <img src=${obj.image}>
             <h2>${obj.name}</h2>
-            <p>${capitalize(obj.role)}</p>
+            <p>${obj.role}</p>
         </article>
     `;
     characterContainer.insertAdjacentHTML("beforeend", myHTML);
@@ -50,58 +50,30 @@ function showDialog() {
 }
 async function showDialogCharacter(article) {
     const selectedCharacterName = article.querySelector("h2").textContent;
-    const characterObj = await findCharacter(selectedCharacterName);
-    const myHTML = /*html*/`
-            <figure>
-                <img src=${characterObj.image}>
-            </figure>
-            <article>
-                <h2>${characterObj.name}</h2>
-                <p id="dialog-quote">${characterObj.quote}</p>
-                <h3>Personal info</h3>
-                <ul>
-                    <li>Birthdate: ${characterObj.birthdate}</li>
-                    <br>
-                    <li>Age: ${getCurrentAge(characterObj.birthdate)}</li>
-                    <br>
-                    <li>Gender: ${capitalize(characterObj.gender)}</li>
-                    <br>
-                    <li>Favorite food/drink: ${capitalize(characterObj.favorite)}</li>
-                    <br>
-                    <li>Role: ${capitalize(characterObj.role)}</li>
-                    <br>
-                    <li>Favorite passtime: ${capitalize(characterObj.passtime)}</li>
-                    <br>
-                    <li>Sport of choice: ${capitalize(characterObj.sport)}</li>
-                </ul>
-            </article>`;
-    dialogContainer.innerHTML = myHTML;
+    const character = await findCharacter(selectedCharacterName);
+    const dialog = document.querySelector("#character-dialog");
+    /* ===== image ===== */
+    dialog.querySelector("figure").innerHTML = /*html*/`
+    <img src="${character.image}">`;
+
+    /* ===== name =====*/
+    dialog.querySelector("#dialog-name").textContent = character.name;
+    
+    /* ===== Character Information ===== */
+    dialog.querySelector("#dialog-occupation").textContent = character.occupation;
+    dialog.querySelector("#dialog-age").textContent = character.age;
+    dialog.querySelector("#dialog-gender").textContent = character.gender;
+    dialog.querySelector("#dialog-haircolor").textContent = character.hairColor;
+    dialog.querySelector("#dialog-religion").textContent = character.religion;
+    dialog.querySelector("#dialog-grade").textContent = character.schoolgrade;
+    dialog.querySelector("#dialog-voicedby").textContent = character.voicedBy;
+    dialog.querySelector("#dialog-episodes").textContent = character.episodes;
+    dialog.querySelector("#dialog-firstappearance").textContent = character.firstAppearance;
+    dialog.querySelector("#dialog-appearances").textContent = character.appearances;
 }
 async function findCharacter(name) {
     const characters = await fetchData();
     for (const obj of characters) {
         if (obj.name === name) return obj;
     }
-}
-
-/* =========== Date/Age functions =========== */
-function formatDate(date) {
-    let dateArr = date.split("-");
-    dateArr = dateArr.reverse();
-    return dateArr.join("-");
-}
-function getCurrentAge(birthdate) {
-    const today = new Date(Date.now());
-    const dateOfBirth = new Date(formatDate(birthdate));
-    if (today.getMonth() < dateOfBirth.getMonth() || (today.getMonth() === dateOfBirth.getMonth() && today.getDate() < dateOfBirth.getDate())) {   
-        return today.getFullYear() - dateOfBirth.getFullYear() - 1;
-    }
-    else {
-        return today.getFullYear() - dateOfBirth.getFullYear();
-    }
-}
-
-/* =========== Text edit functions =========== */
-function capitalize(name) {
-    return name[0].toUpperCase() + name.substring(1).toLowerCase();
 }
