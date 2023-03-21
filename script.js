74fa3d4e5818087ff0b338d9f6e1bc8c13ea11e2
emilvn <emilvnielsen@hotmail.com>
2023-03-21 13:24:20 +0100

    changed format for getting object values

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ function showAllCharacters(data) {
 function showCharacter(obj) {
     const myHTML = /*html*/ `
         <article>
-            <img src=${obj["image"]}>
-            <h2>${obj["name"]}</h2>
-            <p>${capitalize(obj["role"])}</p>
+            <img src=${obj.image}>
+            <h2>${obj.name}</h2>
+            <p>${capitalize(obj.role)}</p>
         </article>
     `;
     characterContainer.insertAdjacentHTML("beforeend", myHTML);
@@ -53,26 +53,26 @@ async function showDialogCharacter(article) {
     const characterObj = await findCharacter(selectedCharacterName);
     const myHTML = /*html*/`
             <figure>
-                <img src=${characterObj["image"]}>
+                <img src=${characterObj.image}>
             </figure>
             <article>
-                <h2>${characterObj["name"]}</h2>
-                <p id="dialog-quote">${characterObj["quote"]}</p>
+                <h2>${characterObj.name}</h2>
+                <p id="dialog-quote">${characterObj.quote}</p>
                 <h3>Personal info</h3>
                 <ul>
-                    <li>Birthdate: ${characterObj["birthdate"]}</li>
+                    <li>Birthdate: ${characterObj.birthdate}</li>
                     <br>
-                    <li>Age: ${getCurrentAge(characterObj["birthdate"])}</li>
+                    <li>Age: ${getCurrentAge(characterObj.birthdate)}</li>
                     <br>
-                    <li>Gender: ${capitalize(characterObj["gender"])}</li>
+                    <li>Gender: ${capitalize(characterObj.gender)}</li>
                     <br>
-                    <li>Favorite food/drink: ${capitalize(characterObj["favorite"])}</li>
+                    <li>Favorite food/drink: ${capitalize(characterObj.favorite)}</li>
                     <br>
-                    <li>Role: ${capitalize(characterObj["role"])}</li>
+                    <li>Role: ${capitalize(characterObj.role)}</li>
                     <br>
-                    <li>Favorite passtime: ${capitalize(characterObj["passtime"])}</li>
+                    <li>Favorite passtime: ${capitalize(characterObj.passtime)}</li>
                     <br>
-                    <li>Sport of choice: ${capitalize(characterObj["sport"])}</li>
+                    <li>Sport of choice: ${capitalize(characterObj.sport)}</li>
                 </ul>
             </article>`;
     dialogContainer.innerHTML = myHTML;
@@ -80,7 +80,7 @@ async function showDialogCharacter(article) {
 async function findCharacter(name) {
     const characters = await fetchData();
     for (const obj of characters) {
-        if (obj["name"] === name) return obj;
+        if (obj.name === name) return obj;
     }
 }
 
