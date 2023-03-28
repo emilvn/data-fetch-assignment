ca6b3d80403697765c2c3cfdef7fbb1f836ce3d1
emilvn <emilvnielsen@hotmail.com>
2023-03-28 14:30:55 +0200

    fixed episodes

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function showCharacter(obj) {
         </article>
         `;
     characterContainer.insertAdjacentHTML("beforeend", myHTML);
-    characterContainer.querySelector("article:last-child").addEventListener("click", () => showDialog(obj));
+    characterContainer.querySelector("article:last-child").addEventListener("click", () => showDialog(obj) );
 }
 /* ========== Dialog function ========== */
 async function showDialog(character) {
